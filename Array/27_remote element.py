c9a0319fc66a58acc03690853a5d364326a726d0
Zhang Duo <zhangduo2015@yahoo.co.jp>
2018-09-24 12:02:22 +0900

    Update 27_remote element.py

--- Array/27_remote element.py
+++ Array/27_remote element.py
@@ -3,8 +3,13 @@ from __future__ import print_function
 # Space: O(1)
 #
 # Given an array and a value, remove all instances of that value in place and return the new length.
-#
+# 给定数组nums和值val，在适当位置删除该值的所有实例并返回新长度。
 # The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+
+# Given nums = [3,2,2,3], val = 3, Your function should return length = 2,[2,2]
+# Given nums = [0,1,2,2,3,0,4,2], val = 2, Your function should return length = 5,[0,1,3,0,4]
+
+
 # 定义两个指针，一个从-1开始，一个从0开始，依次遍历。
 
 class Solution:
@@ -18,7 +23,7 @@ class Solution:
         :rtype: int
         """
         slow = -1
-        for i in xrange(0, len(nums)):
+        for i in range(0, len(nums)):
             if nums[i] != val:
                 slow += 1
                 nums[slow] = nums[i]
