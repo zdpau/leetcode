9bbdbc5e14b2f68639414592d12ff1be65a711c4
Zhang Duo <zhangduo2015@yahoo.co.jp>
2018-09-25 17:07:09 +0900

    Create find-the-celebrity.py

---
+++ Array/277.find-the-celebrity/find-the-celebrity.py
@@ -0,0 +1,23 @@
+# The knows API is already defined for you.
+# @param a, person a
+# @param b, person b
+# @return a boolean, whether a knows b
+# def knows(a, b):
+
+class Solution(object):
+    def findCelebrity(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        cand = 0
+        # Find the candidate.
+        for i in range(1, n):
+            if knows(cand, i):
+                cand = i          # All candidates < i are not celebrity candidates.
+        # Verify the candidate.
+        for i in range(n):
+        ＃
+            if i != cand and knows(cand, i) or not knows(i, cand): # 先ｏｒ后ａｎｄ。
+                return -1　＃如果不是名人，返回-1
+        return cand
