f6305f780fba5ab41bedccc9a80b95e7daf82aaa
Zhang Duo <zhangduo2015@yahoo.co.jp>
2018-11-14 20:27:24 +0900

    Update 备忘录法实现斐波那契.py

--- 备忘录法实现斐波那契.py
+++ 备忘录法实现斐波那契.py
@@ -1,18 +1,1 @@
-from functools import wraps
 
-def memo(func):
-    cache = {}
-    @wraps(func)
-    def wrap(*args):
-        if args not in cache:
-            cache[args] = func(*args)
-        return cache[args] # 之前加了else这一逻辑，其实不对。
-    return wrap
-
-@memo
-def fib(i):
-    if i < 2:
-        return 1
-    return fib(i-1)+fib(i-2)
-
-fib(100)
